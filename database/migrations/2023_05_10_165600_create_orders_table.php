478460a9d29a32d88a859c9ec0f930017d00e5f1
Dwight Lindquist <dwight.lindquist@gmail.com>
2023-05-10 14:43:54 -0400

    can CRUD order objects with thunderclient

---
+++ database/migrations/2023_05_10_165600_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onDelete('cascade'); // if a user is deleted, his child orders are also deleted         
+            $table->float('total_cost', 7, 2);
+            $table->string('address_line_one');
+            $table->string('address_line_two')->nullable();
+            $table->string('city');
+            $table->string('state');
+            $table->integer('postal_code');
+            $table->string('country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
